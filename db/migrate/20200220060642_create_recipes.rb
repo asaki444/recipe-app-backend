3c90bcdd2296c38ab3389fdb3a5f7d3df4790fc2
Aditi <asaki444@gmail.com>
2020-02-20 01:10:20 -0500

    added recipes, and ingredients models, and controllers

--- db/migrate/20200220060642_create_recipes.rb
+++ db/migrate/20200220060642_create_recipes.rb
@@ -1,9 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[5.0]
   def change
     create_table :recipes do |t|
+      t.primary_key :id
       t.string :title
-      t.integer :ingredient_id
-      t.text  :instructions
+      t.text :instructions
 
       t.timestamps
     end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,8 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  it "has a valid input" do 
-    Recipe.create(:title, :ingredient_id).should be_valid
-  end
-
+  pending "add some examples to (or delete) #{__FILE__}"
 end
