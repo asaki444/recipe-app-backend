a875180cca425c3251c8eef6827602958e3ba77e
Aditi <asaki444@gmail.com>
2020-02-22 03:33:46 -0500

    added controller tests

--- spec/controllers/ingredient_controller_spec.rb
+++ spec/controllers/ingredient_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe IngredientController, type: :controller do
+    describe "GET index" do
+        it "has a 200 status code" do
+          expect(response.status).to eq(200)
+        end
 
+        it "returns all ingredients" 
+      end
 end
--- spec/controllers/recipe_controller_spec.rb
+++ spec/controllers/recipe_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe RecipeController, type: :controller do
+    describe "GET index" do
+        it "has a 200 status code" do
+          get :index
+          expect(response.status).to eq(200)
+        end
+      end
 
+    describe "POST #create" do
+        context "with valid attributes" do
+          it "saves the new recipe in the database"
+      end
+    end
+
+    describe "GET #show" do
+        it "assigns the requested recipe"
+        it "renders the :json"
+    end
 end
