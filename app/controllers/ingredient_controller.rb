043fdbb4685a6481ec7a3dd1451538f49a1f0d38
Aditi <asaki444@gmail.com>
2020-02-25 23:34:31 -0500

    added routes

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,2 +1,14 @@
 class IngredientController < ApplicationController
+    def index
+        Ingredients.all
+    end
+    
+    def create(name:)
+        Ingredient.create(name:)
+    end
+
+    def show(id:)
+        Ingredient.find(:id)
+    end
+
 end
