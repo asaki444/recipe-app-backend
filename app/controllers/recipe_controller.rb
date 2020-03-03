6dd53f67c4784f6101899d4cf9fa843f961c9335
Aditi <asaki444@gmail.com>
2020-03-02 23:22:09 -0500

    started create recipe method

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,5 +3,12 @@ class RecipeController < ApplicationController
         Recipe.all
      end
 
+     def create(title, instructions, ingredients)
+        Recipe.create(title: title, instructions: instructions)
+        for i in ingredients.length()
+          Ingredient.create(name: ingredients[i].name)
+        end
+     end
+
     
 end
