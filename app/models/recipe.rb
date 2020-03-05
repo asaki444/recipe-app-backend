f77655e91964ee75b9ff9af90893b7ee3d477dd4
Aditi <asaki444@gmail.com>
2020-03-04 20:23:46 -0500

    Added sessions controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
     validates_presence_of :title, :instructions
-    has_many :users
+    has_and_belongs_to_many :users
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
 end
